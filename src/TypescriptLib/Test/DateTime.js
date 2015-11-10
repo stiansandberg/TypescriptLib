b162dad584993ed9069d8b65d781ffe8177650be
Stian Sandberg <stian@stian.net>
2015-11-10 15:34:18 +0100

    removed DateTime

--- src/TypescriptLib/Test/DateTime.js
+++ src/TypescriptLib/Test/DateTime.js
@@ -1,23 +1,15 @@
-﻿/// <reference path="../datetime.ts" />
-
+/// <reference path="../datetime.ts" />
 QUnit.module('DateTime');
-
-QUnit.test('Today', function (a: QUnitAssert) {
-
+QUnit.test('Today', function (a) {
     var today = DateTime.Today();
     var now = new Date();
     a.ok(now.isSameDay(today.getDate()));
 });
-
 //QUnit.test('', function (a: QUnitAssert) {
 //});
-
 //QUnit.test('', function (a: QUnitAssert) {
 //});
-
 //QUnit.test('', function (a: QUnitAssert) {
 //});
-
 //QUnit.test('', function (a: QUnitAssert) {
 //});
-
